2d55626070be60725864f6c537c5f52256aa1c2e
songanji <songanji@gmail.com>
2014-04-01 23:16:29 +0900

    구조 새로 잡는 작업중

--- edit/js/fn-son-edit.js
+++ edit/js/fn-son-edit.js
@@ -2,20 +2,7 @@
 (function ($) {
 	jQuery.SonJS = {};
 	
-	jQuery.SonJS.markdown = function(text){
-		text = text.replace(/(^\s*)|(\s*$)/g, "" ).replace(/\n/ig, "<br>");
-		text = $.BOLD( text );
-		text = $.ITALIC( text );
-		text = $.UNDERLINING( text );
-		text = $.SUPERSCRIPT( text );
-		text = $.SUBERSCRIPT( text );
-		text = $.H1( text );
-		text = $.HR( text );
-		text = $.FIELD( text );
-		text = $.ALERT( text );
-		text = $.INFO( text );
-		return text;
-	};
+
 	
 	
 })(jQuery);
--- edit/js/fn-son-markdown.js
+++ edit/js/fn-son-markdown.js
@@ -1,57 +1,45 @@
 
-(function ($) {
-	
-	jQuery.BOLD = function(contents){
-		contents = contents.replace(/\*{2}(.*)\*{2}/gm, "<B>$1</B>");
-		return contents;
-	};
-	
-	jQuery.ITALIC = function(contents){
-		contents = contents.replace(/([^http:])\/{2}(.*)\/{2}/igm, "$1<I>$2</I>");
-		return contents;
-	};
-	
-	jQuery.UNDERLINING = function(contents){
-		contents = contents.replace(/\_{2}(.*)\_{2}/gm, "<U>$1</U>");
-		return contents;
-	};
-	
-	jQuery.SUPERSCRIPT = function(contents){
-		contents = contents.replace(/\[sb\](.*)\[sb\]/gm, "<sub>$1</sub>");
-		return contents;
-	};
-	
-	jQuery.SUBERSCRIPT = function(contents){
-		contents = contents.replace(/\[sp\](.*)\[sp\]/gm, "<sup>$1</sup>");
-		return contents;
-	};
-	
-	jQuery.H1 = function(contents){
-		contents = contents.replace(/(h1.(\s{0,})(.*))/gm, "<h1>$3</h1>");
-		contents = contents.replace(/(h2.(\s{0,})(.*))/gm, "<h2>$3</h2>");
-		contents = contents.replace(/(h3.(\s{0,})(.*))/gm, "<h3>$3</h3>");
-		return contents;
-	};
+var SONJS = (function(){
+	var SONJS = {};
+	SONJS.markdown = {};
 
-	jQuery.HR = function(contents){
-		contents = contents.replace(/\[hr\]/gm, "<hr>");
-		return contents;
-	};
-	
-	jQuery.FIELD = function(contents){
-		contents = contents.replace(/\[field(\|(.*?))?\](.*)\[field\]/gm, "<fieldset class=\"fieldset\">\r\n<legend class=\"legend\">$2</legend>\n$3\n</fieldset>");
-		return contents;
-	};
-	
-	jQuery.ALERT = function(contents){
-		contents = contents.replace(/\[alert\](.*)\[alert\]/gm, "<div class=\"box-alert\">$1</div>");
-		return contents;
-	};
-	
-	jQuery.INFO = function(contents){
-		contents = contents.replace(/\[info\](.*)\[info\]/gm, "<div class=\"box-check\">$1</div>");
+	SONJS.markdown = function(contents){
+		contents = contents.replace(/(^\s*)|(\s*$)/g, "" ).replace(/\n/ig, "<br>");
+		contents = SONJS.markdown.BOLD( contents );
+		contents = SONJS.markdown.ITALIC( contents );
+		contents = SONJS.markdown.UNDERLINING( contents );
+		contents = SONJS.markdown.SUPERSCRIPT( contents );
+		contents = SONJS.markdown.SUBERSCRIPT( contents );
+		contents = SONJS.markdown.H1( contents );
+		contents = SONJS.markdown.HR( contents );
+		contents = SONJS.markdown.FIELD( contents );
+		contents = SONJS.markdown.ALERT( contents );
+		contents = SONJS.markdown.INFO( contents );
 		return contents;
 	};
 	
 	
-})(jQuery);
+	SONJS.append = function(){
+		loadJQuery("js/sonjs/sonjs-markup-BOLD.js");
+		loadJQuery("js/sonjs/sonjs-markup-ITALIC.js");
+		loadJQuery("js/sonjs/sonjs-markup-UNDERLINING.js");
+		loadJQuery("js/sonjs/sonjs-markup-SUPERSCRIPT.js");
+		loadJQuery("js/sonjs/sonjs-markup-SUBERSCRIPT.js");
+		loadJQuery("js/sonjs/sonjs-markup-H1.js");
+		loadJQuery("js/sonjs/sonjs-markup-HR.js");
+		loadJQuery("js/sonjs/sonjs-markup-FIELD.js");
+		loadJQuery("js/sonjs/sonjs-markup-ALERT.js");
+		loadJQuery("js/sonjs/sonjs-markup-INFO.js");
+	};
+	SONJS.append();
+	
+	return SONJS;
+})();
+
+function loadJQuery(src) {
+    var oScript = document.createElement("script");
+    oScript.type = "text/javascript";
+    oScript.charset = "utf-8";		  
+    oScript.src = src;	
+    document.getElementsByTagName("head")[0].appendChild(oScript);
+}
