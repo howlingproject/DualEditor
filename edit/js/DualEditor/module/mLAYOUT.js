f4ea309ede75a571de720f2f301c08aca12b7f7b
songanji <songanji@gmail.com>
2015-03-08 16:38:03 +0900

    버그 수정

--- edit/js/DualEditor/module/mLAYOUT.js
+++ edit/js/DualEditor/module/mLAYOUT.js
@@ -1,5 +1,4 @@
 DualEditor.markup.LAYOUT = function(contents){
-    console.info(contents);
     var pattern = /^\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]/igm;
     contents = contents.replace(pattern, "<div class=\"row\"><div class=\"col-md-3\">$1</div><div class=\"col-md-3\">$2</div><div class=\"col-md-3\">$3</div><div class=\"col-md-3\">$4</div></div>");
     pattern.compile(/^\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]/igm);
