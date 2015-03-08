1dd2db0bcae962b5588c086aebd9fae1a4622cef
songanji <songanji@gmail.com>
2015-03-08 16:26:56 +0900

    버그 수정

--- edit/js/DualEditor/module/mLAYOUT.js
+++ edit/js/DualEditor/module/mLAYOUT.js
@@ -1,10 +1,10 @@
 DualEditor.markup.LAYOUT = function(contents){
     console.info(contents);
     var pattern = /^\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]/igm;
-    contents = contents.replace(pattern, "<div class=\"row\">\r\n<div class=\"col-md-3\">\r\n$1\r\n</div>\r\n<div class=\"col-md-3\">\r\n$2\r\n</div>\r\n<div class=\"col-md-3\">\r\n$3\r\n</div>\r\n<div class=\"col-md-3\">\r\n$4\r\n</div>\r\n</div>\r\n");
+    contents = contents.replace(pattern, "<div class=\"row\"><div class=\"col-md-3\">$1</div><div class=\"col-md-3\">$2</div><div class=\"col-md-3\">$3</div><div class=\"col-md-3\">$4</div></div>");
     pattern.compile(/^\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]/igm);
-    contents = contents.replace(pattern, "<div class=\"row\">\r\n<div class=\"col-md-4\">\r\n$1\r\n</div>\r\n<div class=\"col-md-4\">\r\n$2\r\n</div>\r\n<div class=\"col-md-4\">\r\n$3\r\n</div>\r\n</div>\r\n");
+    contents = contents.replace(pattern, "<div class=\"row\"><div class=\"col-md-4\">$1</div><div class=\"col-md-4\">$2</div><div class=\"col-md-4\">$3</div></div>");
     pattern.compile(/^\[layout\](.*?)\[layout\]\n\[layout\](.*?)\[layout\]/igm);
-    contents = contents.replace(pattern, "<div class=\"row\">\r\n<div class=\"col-md-6\">\r\n$1\r\n</div>\r\n<div class=\"col-md-6\">\r\n$2\r\n</div>\r\n</div>\r\n");
+    contents = contents.replace(pattern, "<div class=\"row\"><div class=\"col-md-6\">$1</div><div class=\"col-md-6\">$2</div></div>");
     return contents;
 }
